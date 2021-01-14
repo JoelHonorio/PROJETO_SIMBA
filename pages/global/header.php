4f827ed4ac4123d962198bfab2c1d68a3eb2111e
JoelHonorio <joel.honorio.r34@gmail.com>
2021-01-13 23:28:57 -0300

    Tornando o Login PHP

---
+++ pages/global/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simba</title>
+
+        <link rel="shortcut icon" href="../../assets/img/global/logo.png" type="image/x-icon">
+        <link rel="stylesheet" href="../../assets/css/login/login.css">
+        <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    </head>
+    <body class="text-center">
---
+++ pages/login/login.php
@@ -0,0 +1,27 @@
+<?php
+	include_once '../global/header.php';
+?>
+
+<main class="form-signin">
+    <form>
+        <img class="mb-2" src="../../assets/img/login/logo.svg" width="230" height="230">
+
+        <label for="inputEmail" class="visually-hidden">E-mail</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required>
+
+        <label for="inputPassword" class="visually-hidden">Senha</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="Lembrar"> Lembrar-me
+            </label>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Logar</button>
+    </form>
+</main>
+
+<?php
+	include_once '../global/footer.php';
+?>
